297c8803391a0b9411e77cf3c979f53c5cc48a74
kangta123 <kang7213@gmail.com>
2021-04-06 17:52:39 +0800

    install istio deps

--- docs/helm/install.sh
+++ docs/helm/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ns=${1:-hawk}
 
 kubectl apply -f account.yaml
@@ -8,5 +7,17 @@ secretpodname=`kubectl -n kube-system get secret |grep hawk-token | head -n1 | a
 token=`kubectl -n kube-system get secret ${secretpodname} -o jsonpath={.data.token} | base64 -d`
 apiserver=`kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}'`
 
-helm install --create-namespace --namespace $ns --set hawk.kubernetes.token=$token --set hawk.kubernetes.apiServer=$apiserver hawk ./ 
+helm install --create-namespace --namespace $ns --set hawk.kubernetes.token=$token --set hawk.kubernetes.apiServer=$apiserver hawk ./
 
+
+echo "install istio deps..."
+
+istions="istio-system"
+kubectl create ns ${istions}
+
+helm install --set global.jwtPolicy=first-party-jwt istio-base ./istio/base -n ${istions}
+helm install --set global.jwtPolicy=first-party-jwt istiod ./istio/istio-control/istio-discovery -n ${istiod}
+helm install --set global.jwtPolicy=first-party-jwt istio-ingress ./istio/gateways/istio-ingress -n ${istiod}
+helm install --set global.jwtPolicy=first-party-jwt istio-egress ./istio/gateways/istio-egress -n ${istiod}
+
+kubectl label namespace default istio-injection=enabled
